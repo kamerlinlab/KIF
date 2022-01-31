1d2cb08b8dbe26caab01a122a4865e46d69b6ffd
Rory Crean <rory.m.crean@gmail.com>
2022-01-31 16:26:50 +0100

    Create setup.py

---
+++ key_interactions_finder/setup.py
@@ -0,0 +1,33 @@
+
+# TODO, example taken from encodermap.
+
+
+# from setuptools import setup
+# import os
+
+# with open("description.md", "r") as fh:
+#     long_description = fh.read()
+
+# version_file = open(os.path.join("encodermap", 'VERSION'))
+# version = version_file.read().strip()
+
+# setup(name='encodermap',
+#       version=version,
+#       description='python library for dimensionality reduction',
+#       long_description=long_description,
+#       long_description_content_type="text/markdown",
+#       author='Tobias Lemke',
+#       url="https://github.com/AG-Peter/encodermap",
+#       packages=['encodermap'],
+#       install_requires=[
+#           'numpy',
+#           'matplotlib',
+#           'scipy',
+#           'MDAnalysis',
+#           'tqdm'
+#       ],
+#       classifiers=[
+#           "Programming Language :: Python :: 3",
+#           "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+#           "Operating System :: OS Independent",
+#       ])
