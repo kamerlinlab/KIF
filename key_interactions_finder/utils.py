b7bac9f7679bcbb28f0ffbea099e501b0fbe2a4a
Rory Crean <rory.m.crean@gmail.com>
2022-04-09 19:17:22 +0200

    Update utils.py
    Completed "per_residue_distance_to_site" function.

--- key_interactions_finder/utils.py
+++ key_interactions_finder/utils.py
@@ -2,11 +2,73 @@
 Random helper functions.
 TODO - list functions with mini description here.
 """
+from typing import Optional
+import csv
 import os
 import pandas as pd
 import numpy as np
 from MDAnalysis.analysis import distances
 import MDAnalysis as mda
+
+
+def per_residue_distance_to_site(pdb_file: str,
+                                 site_defintion: str,
+                                 first_residue: int,
+                                 last_residue: int,
+                                 out_file: Optional[str]
+                                 ) -> dict:
+    """
+    Calculate the closest heavy atom distance of each residue to an mdtraj defined
+    selection of a site of interest.
+
+    Parameters
+    ----------
+    pdb_file : str
+        Path to pdb file to use for the distance calculation.
+
+    out_file : Optional[str]
+        Path to output file to write out the per residue distances.
+
+    site_defintion : str
+        mdtraj compatable defintion of the site of interest
+        (i.e. binding site, active site etc..)
+        See here for examples: https://mdtraj.org/1.9.3/atom_selection.html
+
+    first_residue : int
+        First residue to measure the distance from.
+
+    last_residue : int
+        Last residue to measure the distance to.
+
+    Returns
+    ----------
+    dict
+        Residue numbers are the keys and minimum distances are the values.
+    """
+    universe = mda.Universe(pdb_file)
+
+    group2 = universe.select_atoms(site_defintion)
+
+    min_dists = {}
+    for residue in range(first_residue, last_residue+1):
+        selection_str = "not name H* and resid " + str(residue)
+        group1 = universe.select_atoms(selection_str)
+
+        res_dist_arr = distances.distance_array(
+            group1.positions, group2.positions, box=universe.dimensions)
+
+        min_res_dist = np.round(res_dist_arr.min(), 2)
+        min_dists.update({residue: min_res_dist})
+
+    if out_file is None:
+        return min_dists
+
+    with open(out_file, "w", newline="") as file_out:
+        csv_out = csv.writer(file_out)
+        csv_out.writerow(["Residue Number", "Minimum Distance"])
+        csv_out.writerows(min_dists.items())
+        print(f"{out_file} written to disk.")
+    return min_dists
 
 
 def _prep_out_dir(out_dir: str) -> str:
@@ -51,7 +113,6 @@ def _filter_features_by_strings(dataset: pd.DataFrame, strings_to_preserve: list
     pd.DataFrame
         Dataframe with features filtered to only include those the user wants.
     """
-    # Build the search term
     str_rep = "Target|"  # supervised/unsuperivsed learning agnostic.
     for list_item in strings_to_preserve:
         str_rep += list_item + "|"
@@ -60,60 +121,3 @@ def _filter_features_by_strings(dataset: pd.DataFrame, strings_to_preserve: list
     str_rep = str_rep[:-1]
 
     return dataset.loc[:, dataset.columns.str.contains(str_rep)]
-
-
-def per_residue_distance_to_site(
-    pdb_file: str,
-    out_file: str,
-    site_defintion: str,
-    first_residue: int,
-    last_residue: int
-) -> np.ndarray:
-    """
-    Calculate the distance of the CA carbon of each residue to an mdtraj defined
-    selection of a site of interest
-
-    Uses mdtraj under the hood.
-
-    Parameters
-    ----------
-    pdb_file : str
-        Path to pdb file to use for distance calculation.
-
-    out_file : str [Optional]
-        Path to output file to write out per residue distances.
-
-    site_defintion : str
-        mdtraj compatable defintion of the site of interest
-        (i.e. binding site, active site etc..)
-        See here for examples: https://mdtraj.org/1.9.3/atom_selection.html
-
-    first_residue : int
-        First residue to measure the distance from.
-
-    last_residue : int
-        Last residue to measure the distance to.
-
-    Returns
-    ----------
-    np.ndarray
-        1D array of each residues distance.
-
-    """
-    # TODO, test
-    universe = mda.Universe(pdb_file)
-
-    group2 = universe.select_atoms(site_defintion)
-
-    min_dists = []
-    for residue in range(first_residue, last_residue+1):
-        selection_str = "not name H* and resid " + str(residue)
-        group1 = universe.select_atoms(selection_str)
-
-        res_dist_arr = distances.distance_array(
-            group1.positions, group2.positions, box=universe.dimensions)
-
-        min_res_dist = np.round(res_dist_arr.min(), 2)
-        min_dists.append(min_res_dist)
-
-    return min_dists
