db868e256ed7ca826e17677886b02f6d8369925f
Rory Crean <rory.m.crean@gmail.com>
2022-06-29 13:52:55 +0200

    Update utils.py
    Combine measure distance to site of interest methods together.
    Still a little too much repetition perhaps but probably okay as no need to make this method any more fancy in future.

--- key_interactions_finder/utils.py
+++ key_interactions_finder/utils.py
@@ -15,6 +15,7 @@ def per_residue_distance_to_site(pdb_file: str,
                                  site_defintion: str,
                                  first_residue: int,
                                  last_residue: int,
+                                 side_chain_only: bool = False,
                                  out_file: Optional[str] = None,
                                  ) -> dict:
     """
@@ -37,68 +38,10 @@ def per_residue_distance_to_site(pdb_file: str,
     last_residue : int
         Last residue to measure the distance to.
 
-    out_file : Optional[str]
-        Path to output file to write out data.
-
-    Returns
-    ----------
-    dict
-        Residue numbers are the keys and minimum distances are the values.
-    """
-    universe = mda.Universe(pdb_file)
-
-    group2 = universe.select_atoms(site_defintion)
-
-    min_dists = {}
-    for residue in range(first_residue, last_residue+1):
-        selection_str = "not name H* and resid " + str(residue)
-        group1 = universe.select_atoms(selection_str)
-
-        res_dist_arr = distances.distance_array(
-            group1.positions, group2.positions, box=universe.dimensions)
-
-        min_res_dist = np.round(res_dist_arr.min(), 2)
-        min_dists.update({residue: min_res_dist})
-
-    if out_file is None:
-        return min_dists
-
-    with open(out_file, "w", newline="") as file_out:
-        csv_out = csv.writer(file_out)
-        csv_out.writerow(["Residue Number", "Minimum Distance"])
-        csv_out.writerows(min_dists.items())
-        print(f"{out_file} written to disk.")
-    return min_dists
-
-
-def per_residue_side_chain_distance_to_site(pdb_file: str,
-                                            site_defintion: str,
-                                            first_residue: int,
-                                            last_residue: int,
-                                            out_file,
-                                            ) -> dict:
-    """
-    Calculate the closest heavy atom distance of each residue's side chain to an mdtraj defined
-    selection of a site of interest. You can write the results to file if desired.
-
-    Glycine residues of course do not have a side chain,
-    so in the case of a glycine residue the CA atom is used instead.
-
-    Parameters
-    ----------
-    pdb_file : str
-        Path to pdb file to use for the distance calculation.
-
-    site_defintion : str
-        mdtraj compatable defintion of the site of interest
-        (i.e. binding site, active site etc..)
-        See here for examples: https://mdtraj.org/1.9.3/atom_selection.html
-
-    first_residue : int
-        First residue to measure the distance from.
-
-    last_residue : int
-        Last residue to measure the distance to.
+    side_chain_only: bool = False,
+        Choose whether you want to measure the minimum distance using only the side chain of each residue.
+        If true, only the side chain atoms are used. For glycines (which do not have a side chain),
+        the CA of the glycine is used instead.
 
     out_file : Optional[str]
         Path to output file to write out data.
@@ -109,32 +52,43 @@ def per_residue_side_chain_distance_to_site(pdb_file: str,
         Residue numbers are the keys and minimum distances are the values.
     """
     universe = mda.Universe(pdb_file)
-
     group2 = universe.select_atoms(site_defintion)
-
     min_dists = {}
-    for residue in range(first_residue, last_residue+1):
-        selection_str = "not backbone and not name H* and resid " + \
-            str(residue)
-        group1 = universe.select_atoms(selection_str)
 
-        res_dist_arr = distances.distance_array(
-            group1.positions, group2.positions, box=universe.dimensions)
+    if side_chain_only:
+        for residue in range(first_residue, last_residue+1):
+            selection_str = "not backbone and not name H* and resid " + \
+                str(residue)
+            group1 = universe.select_atoms(selection_str)
 
-        try:
-            min_res_dist = np.round(res_dist_arr.min(), 2)
+            res_dist_arr = distances.distance_array(
+                group1.positions, group2.positions, box=universe.dimensions)
 
-        except ValueError:  # zero-size array to reduction operation minimum which has no identity
-            # This happens for glycines which have no side chain...
-            selection_str = "name CA and resid " + str(residue)
+            try:
+                min_res_dist = np.round(res_dist_arr.min(), 2)
+
+            except ValueError:  # catches "zero-size array to reduction operation minimum which has no identity"
+                # This happens for glycines which have no side chain...
+                selection_str = "name CA and resid " + str(residue)
+                group1 = universe.select_atoms(selection_str)
+
+                res_dist_arr = distances.distance_array(
+                    group1.positions, group2.positions, box=universe.dimensions)
+
+                min_res_dist = np.round(res_dist_arr.min(), 2)
+
+            min_dists.update({residue: min_res_dist})
+
+    else:  # both side and main chain route.
+        for residue in range(first_residue, last_residue+1):
+            selection_str = "not name H* and resid " + str(residue)
             group1 = universe.select_atoms(selection_str)
 
             res_dist_arr = distances.distance_array(
                 group1.positions, group2.positions, box=universe.dimensions)
 
             min_res_dist = np.round(res_dist_arr.min(), 2)
-
-        min_dists.update({residue: min_res_dist})
+            min_dists.update({residue: min_res_dist})
 
     if out_file is None:
         return min_dists
